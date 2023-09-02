adae3c1d73e7e7931e280f98194cc51bd3720779
asriel <barath_cc@ece.iitr.ac.in>
2023-09-03 02:47:07 +0530

    homepage integration is working

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,24 +5,34 @@ import Shopingcart from './shopingcart'
 import Search from './search';
 import {useEffect, useContext} from "react";
 import { fetchDataFromApi} from "../utils/api";
-
 import  {Context} from "../utils/context.js";
 
 
 export default function Navbar() {
 
-const { categories, setCategories} = useContext(Context);  
+
+const { categories, setCategories , products, setProducts} = useContext(Context);  
 useEffect(() => {
   getCategories();
 }, []);
 
+
 const getCategories = () => {
-  fetchDataFromApi("/api/categories?populate=*").then((res) => { 
-    console.log(res);
-    setCategories(res);
-  });
+
+  fetchDataFromApi("/api/categories?populate=*")
+    .then((res) => {
+      console.log(res);
+      setCategories(res);
+    })
+    .catch((error) => {
+      console.error("Error fetching categories:", error);
+    });
 
 };
+
+
+
+
 
   const[showCart,setShowCart]=useState(false);
   const[showSearch,setShowSearch]=useState(false);
@@ -44,9 +54,22 @@ const getCategories = () => {
     <header className='nav'>
     <div className='navcontent'>
         <div className='left'>
-        <ul>
         
+        
+        <ul>
+        {categories ? (
+      categories.data.map((category, index) => (
+        <React.Fragment key={category.id}>
+          <li>{category.attributes.title}</li>
+          {index < categories.data.length - 1 && <li>|</li>}
+        </React.Fragment>
+      ))
+    ) : (
+      <li>Loading...</li>
+    )}
   </ul>
+
+
         </div>
         <div className='mid'>
         Green-Com
--- src/components/products.js
+++ src/components/products.js
@@ -1,20 +1,53 @@
 import React from 'react'
 import './products.css'
 import Product from './product'
+import {useEffect, useContext} from "react";
+import { fetchDataFromApi} from "../utils/api";
+import  {Context} from "../utils/context.js";
+import axios from "axios";
+
 
 export default function Products() {
+  
+  const { categories, setCategories , products, setProducts} = useContext(Context);  
+  
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    
+    fetchDataFromApi("/api/products?populate=*")
+      .then((res) => {
+        console.log(res);
+        setProducts(res);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  
+  };
+  
+
   return (
     <div className="productscontainer">
      fish  off
     <div className="products">
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
+    
+    {products ? (
+      
+      products.data.slice(0,8).map((product, index) => (
+        <div key={product.id}>
+          <Product pho={"http://localhost:1337" + product.attributes.img.data[0].attributes.url} productName={product.attributes.title} 
+          desc={product.attributes.desc} price={product.attributes.Price} />
+        </div>
+      ))
+    ) : (
+      <li>Loading...</li>
+    )}
+    
+    
+    
 
     </div>
     </div>
